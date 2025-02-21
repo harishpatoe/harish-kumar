0cba518a102b3f4f48b11dc4f2c7f88f7f8f00da
harishpatoe <harishpatoe@gmail.com>
2025-02-21 16:52:05 +0530

    intial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router";
+// import './index.css'
 import App from './App.jsx'
+import Login from './Pages/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <div>
+    <BrowserRouter>
+    {/* <Header /> */}
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} /> 
+    </Routes>
+    {/* <Footer /> */}
+    </BrowserRouter>
+  </div>
+  
 )
