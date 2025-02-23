72ef8218c3be74c27bc475f2b74652e93b7cbf41
harishpatoe <harishpatoe@gmail.com>
2025-02-24 02:08:59 +0530

    intial commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+
 const Login = () => {
   return (
     <div>
-      Login
+      <h1>Login</h1>
+      <form>
+        <label htmlFor="email">E-Mail</label>
+        <input type="email" id="email" name="email" placeholder='Enter E-Mail'/>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" placeholder='Enter Password'/>
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
