0de69552cd518c893548b2cda5112decd64e81ae
harishpatoe <harishpatoe@gmail.com>
2025-02-25 04:30:21 +0530

    intial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
 import Login from './Pages/Login.jsx'
+import Register from './Pages/Register.jsx'
+import NU from './Pages/NU.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <div>
@@ -12,6 +16,8 @@ createRoot(document.getElementById('root')).render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/login" element={<Login />} /> 
+      <Route path="/register" element={<Register />} /> 
+      <Route path="/nu" element={<NU/>} /> 
     </Routes>
     {/* <Footer /> */}
     </BrowserRouter>
