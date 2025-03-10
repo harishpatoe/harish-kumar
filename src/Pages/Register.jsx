abff412681031968874c219ca1779994cdebf854
harishpatoe <harishpatoe@gmail.com>
2025-03-10 17:17:08 +0530

    initial commit

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -7,84 +7,86 @@ const onFinish = (values) => {
 const Register = () => (
   
   <div className="flex flex-col items-center justify-center min-h-screen p-4">
-    <h1 className='text-4xl py-5 text-center'>Register</h1>
-    <Form
-      // name="basic"
-      onFinish={onFinish}
-      // onFinishFailed={onFinishFailed}
-      // autoComplete="off"
-    >
-      {/* Username */}
-      <div className='mx-10'>
-        <Form.Item
-          label="Username"
-          name="Username"
-        >
-          <Input placeholder='Enter your Username'/>
-        </Form.Item>
-      </div>
+    <div className="bg-white border-2 border-black rounded-lg shadow-md p-8">
+        <h1 className='text-4xl py-5 text-center'>Register</h1>
+      <Form
+        // name="basic"
+        onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        // autoComplete="off"
+      >
+        {/* Username */}
+        <div className='mx-10'>
+          <Form.Item
+            label="Username"
+            name="Username"
+          >
+            <Input placeholder='Enter your Username'/>
+          </Form.Item>
+        </div>
 
-      {/* E-mail */}
-      <div className='mx-10'>
-        <Form.Item
-          label="E-mail"
-          name="E-mail"
-        >
-          <Input placeholder='Enter your E-mail'/>
-        </Form.Item>
-      </div>
-      
-      {/* Mobile No. */}
-      <div className='mx-10'>
-        <Form.Item
-          label="Mobile No."
-          name="Mobile No."
-        >
-          <Input placeholder='Enter your Mobile No.'/>
-        </Form.Item>
-      </div>
+        {/* E-mail */}
+        <div className='mx-10'>
+          <Form.Item
+            label="E-mail"
+            name="E-mail"
+          >
+            <Input placeholder='Enter your E-mail'/>
+          </Form.Item>
+        </div>
 
-      {/* Password */}
-      <div className='mx-10'>
-        <Form.Item
-          label="Password"
-          name="Password"
-        >
-          <Input.Password placeholder='Enter your Password'/>
+        {/* Mobile No. */}
+        <div className='mx-10'>
+          <Form.Item
+            label="Mobile No."
+            name="Mobile No."
+          >
+            <Input placeholder='Enter your Mobile No.'/>
+          </Form.Item>
+        </div>
+
+        {/* Password */}
+        <div className='mx-10'>
+          <Form.Item
+            label="Password"
+            name="Password"
+          >
+            <Input.Password placeholder='Enter your Password'/>
+          </Form.Item>
+        </div>
+
+        {/* Confirm Password */}
+        <div className='mx-10'>
+          <Form.Item
+            label=" Confirm Password"
+            name="Confirm Password"
+          >
+            <Input.Password placeholder='Enter your Confirm Password'/>
+          </Form.Item>
+        </div>
+
+        {/* Reset */}
+        <Form.Item label={null} className='w-full place-items-center'>
+          <Button type="link" htmlType="Reset?" className='w-full'>
+            Reset
+          </Button>
         </Form.Item>
-      </div>
 
-      {/* Confirm Password */}
-      <div className='mx-10'>
-        <Form.Item
-          label=" Confirm Password"
-          name="Confirm Password"
-        >
-          <Input.Password placeholder='Enter your Confirm Password'/>
+        {/* Checkbox */}
+        <div className='mx-10'>
+          <Form.Item name="Registered" valuePropName="checked" label={null}>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+        </div>
+
+        {/* Register Button */}
+        <Form.Item label={null} className='w-full place-items-center'>
+          <Button type="primary" htmlType="Register" className='w-full'>
+            Register
+          </Button>
         </Form.Item>
-      </div>
-
-      {/* Reset */}
-      <Form.Item label={null} className='w-full place-items-center'>
-        <Button type="link" htmlType="Reset?" className='w-full'>
-          Reset
-        </Button>
-      </Form.Item>
-
-      {/* Checkbox */}
-      <div className='mx-10'>
-        <Form.Item name="Registered" valuePropName="checked" label={null}>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-      </div>
-
-      {/* Register Button */}
-      <Form.Item label={null} className='w-full place-items-center'>
-        <Button type="primary" htmlType="Register" className='w-full'>
-          Register
-        </Button>
-      </Form.Item>
-    </Form>
+      </Form>
+    </div>
   </div>
 );
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import './index.css'
 import App from './App.jsx'
 import Login from './Pages/Login.jsx'
 import Register from './Pages/Register.jsx'
-import NU from './Pages/NU.jsx';
-import Task from './Pages/Task.jsx';
 
 
 createRoot(document.getElementById('root')).render(
@@ -18,8 +16,6 @@ createRoot(document.getElementById('root')).render(
       <Route path="/" element={<App />} />
       <Route path="/login" element={<Login />} /> 
       <Route path="/register" element={<Register />} /> 
-      <Route path="/nu" element={<NU/>} /> 
-      <Route path="/task" element={<Task/>} /> 
     </Routes>
     {/* <Footer /> */}
     </BrowserRouter>
